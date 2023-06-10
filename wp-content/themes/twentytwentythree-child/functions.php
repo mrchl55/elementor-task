a65921077c5e600ce749d5978827d5cc3d9126c2
mrchl55 <37666744+mrchl55@users.noreply.github.com>
2023-06-10 23:24:09 +0200

    disable admin bar for wp-test

--- wp-content/themes/twentytwentythree-child/functions.php
+++ wp-content/themes/twentytwentythree-child/functions.php
@@ -9,3 +9,13 @@ function twentytwentythree_styles()
 }
 
 add_action('wp_enqueue_scripts', 'twentytwentythree_styles');
+
+function remove_admin_bar_for_wp_test()
+{
+    $current_user = wp_get_current_user();
+    if ($current_user->user_login === 'wp-test') {
+        show_admin_bar(false);
+    }
+}
+
+add_action('after_setup_theme', 'remove_admin_bar_for_wp_test');
